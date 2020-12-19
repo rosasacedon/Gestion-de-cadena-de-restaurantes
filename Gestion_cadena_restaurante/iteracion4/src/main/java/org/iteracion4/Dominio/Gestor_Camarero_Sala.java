432c605bcf91aea314ead11718f422df79154cf9
Victor Martín Alonso <vic.sax.29@gmail.com>
2020-12-19 16:35:40 +0100

    Añadir obtener_comidas

--- Gestion_cadena_restaurante/iteracion4/src/main/java/org/iteracion4/Dominio/Gestor_Camarero_Sala.java
+++ Gestion_cadena_restaurante/iteracion4/src/main/java/org/iteracion4/Dominio/Gestor_Camarero_Sala.java
@@ -1,24 +1,32 @@
 package org.iteracion4.Dominio;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Gestor_Camarero_Sala {
 
-	public void solicitar_cuenta() {
+	public Cuenta solicitar_cuenta() {
 		ArrayList<String> bebidas =new ArrayList<>();
 		bebidas = obtener_bebidas();
 		ArrayList<String> comidas =new ArrayList<>();
 		comidas = obtener_comidas();
 		Cuenta cuenta= new Cuenta(1,bebidas, comidas);
+		
+		return cuenta;
 	}
+	@SuppressWarnings("resource")
 	private ArrayList<String> obtener_comidas() {
-		// TODO Auto-generated method stub
-		return null;
+		Scanner sc = new Scanner(System.in);
+		Mesa m = new Mesa();
+		ArrayList<String> comidas=new ArrayList<String>();
+		for (int i=0; i< m.getNum_comensales_int(); i++) {
+			System.out.println("Introduzca la comida:");
+			String comida = sc.nextLine();
+			comidas.add(comida);
+		}
+		return comidas;
 	}
-	private ArrayList<String> obtener_bebidas() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
 	public void marcar_estado_mesa(String opcion) {
 		Mesa mesa = new Mesa();
 		switch (opcion) {
