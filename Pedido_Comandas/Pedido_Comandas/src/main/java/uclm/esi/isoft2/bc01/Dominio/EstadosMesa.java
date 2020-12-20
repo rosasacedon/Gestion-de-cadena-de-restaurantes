6524be5d72251165ccd9e4d577ec7855920f0f0a
Rosa M. Sacedón Ortega <57213654+rosasacedon@users.noreply.github.com>
2020-12-20 12:09:24 +0100

    Delete EstadosMesa.java

--- Pedido_Comandas/Pedido_Comandas/src/main/java/uclm/esi/isoft2/bc01/Dominio/EstadosMesa.java
+++
@@ -1,6 +0,0 @@
-package uclm.esi.isoft2.bc01.Dominio;
-
-public class EstadosMesa {
-
-public enum Estados { Libre, Reservada, Ocupada, pidiendo, espera_comida, servida, espera_cuenta, pagando, preparacion}
-}
